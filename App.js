fb9ca4404b01059468cf3cf136e55df13db10a54
Azzidahaka <Azzidahaka@gmail.com>
2024-06-19 03:46:47 -0600

    use netinfo to check user network status

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, disableNetwork, enableNetwork } from 'firebase/firestore';
+import { useNetInfo } from '@react-native-community/netinfo'; //used to get  network connection state
+import { useEffect } from 'react';
+import { LogBox, Alert } from 'react-native';
 import Start from './components/Start';
 import Chat from './components/Chat';
 
 const Stack = createNativeStackNavigator();
-
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
-import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['AsyncStorage has been extracted from']); //Dismiss async deprecated warning
 
 export default function App() {
@@ -20,12 +20,20 @@ export default function App() {
     messagingSenderId: '750788508120',
     appId: '1:750788508120:web:30d75caad942c84fc51aad',
   };
-
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-
   // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
+  const connectionStatus = useNetInfo();
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert('Connection Lost!');
+      //stop firebase from attempting to connect to db when offline
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
@@ -36,6 +44,7 @@ export default function App() {
         <Stack.Screen name='Chat'>
           {(props) => (
             <Chat
+              isConnected={connectionStatus.isConnected}
               db={db}
               {...props}
             />
--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from 'react-native';
-import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 
 import { collection, getDocs, addDoc, query, onSnapshot, orderBy } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
   const { userID, name, color } = route.params;
   const [messages, setMessages] = useState([]);
 
@@ -23,35 +23,51 @@ const Chat = ({ route, navigation, db }) => {
     }
   };
 
+  //load messages storedn in asyncstorage
+  const loadCachedMessages = async () => {
+    const cachedMessages = (await AsyncStorage.getItem('messages')) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
+  //checkes if user is offline and disable the input accordingly
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  };
   useEffect(() => {
     navigation.setOptions({ title: name });
-    //query the message collection in firebase and sort by the createddate in descending order
-    const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
-    //take a snapshot of collection at momment its called
-    const unsubMessages = onSnapshot(q, (documentsSnapshot) => {
-      let newMessages = [];
-      documentsSnapshot.forEach((doc) => {
-        newMessages.push({
-          _id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis()), //convert firebase time stamp object to format for GiftedChat
+    let unsubMessages;
+    if (isConnected === true) {
+      // unregister current onSnapshot() listener to avoid registering multiple listeners when useEffect code is re-executed.
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+      //query the message collection in firebase and sort by the createddate in descending order
+      const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+      //take a snapshot of collection at momment its called
+      unsubMessages = onSnapshot(q, (documentsSnapshot) => {
+        let newMessages = [];
+        documentsSnapshot.forEach((doc) => {
+          newMessages.push({
+            _id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()), //convert firebase time stamp object to format for GiftedChat
+          });
         });
+        cacheMessages(newMessages);
+        setMessages(newMessages);
       });
-      cacheMessages(newMessages);
-      setMessages(newMessages);
-    });
-
+    } else loadCachedMessages();
     //clean up code
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, []);
+  }, [isConnected]);
 
   return (
     <View style={[styles.container, { backgroundColor: color }]}>
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
         user={{
           _id: userID,
